ecad571ced9e15df280f4d30031100ddbeb88992
taihuynhz <129383417+taihuynhz@users.noreply.github.com>
2023-04-23 14:56:59 +0700

    IComparableShape

---
+++ CircleComparer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IComparableShape
+{
+    public class CircleComparator : IComparer<Circle>
+    {
+        public int Compare(Circle c1, Circle c2)
+        {
+            if (c1.getRadius() > c2.getRadius()) return 1;
+            else if (c1.getRadius() < c2.getRadius()) return -1;
+            else return 0;
+        }
+        public int Compare(Rectangle r1, Rectangle r2)
+        {
+            if (r1.getWidth() > r2.getWidth()) return 1;
+            else if (r1.getWidth() < r2.getWidth()) return -1;
+            else return 0;
+
+            if (r1.getLength() > r2.getLength()) return 1;
+            else if (r1.getLength() < r2.getLength()) return -1;
+            else return 0;
+        }
+    }
+}
